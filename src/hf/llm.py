3d824047866b048d7d21273aa3a92ed9cd807f94
Suraj <suri1155be@gmail.com>
2024-07-08 16:04:42 +0800

    Add files via upload

--- src/hf/llm.py
+++ src/hf/llm.py
@@ -1,1 +1,11 @@
+from transformers import pipeline
 
+# pipe = pipeline(task="sentiment-analysis")
+
+pipe = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+def generate_response(prompt:str):
+   response = pipe("This is a great tutorial!")
+   label = response[0]["label"]
+   score = response[0]["score"]
+   return f"The '{prompt}' input is {label} with a score of {score}"
