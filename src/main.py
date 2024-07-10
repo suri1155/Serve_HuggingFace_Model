84d3b6ef6f26311192222aad6e565a490a876224
Suraj <suri1155be@gmail.com>
2024-07-10 15:46:06 +0800

    Add files via upload
    updated

--- src/main.py
+++ src/main.py
@@ -6,16 +6,18 @@ from hf.llm import generate_response
 # We define the app
 app = FastAPI()
 
+
 # We define that we expect our input to be a string
 class RequestModel(BaseModel):
-   input: str
+    input: str
+
 
 # Now we define that we accept post requests
 @app.post("/sentiment")
 def get_response(request: RequestModel):
-   # return {"hi there"}
-   prompt = request.input
-   response = generate_response(prompt)
-   label = response[0]["label"]
-   score = response[0]["score"]
-   return f"The '{prompt}' input is {label} with a score of {score}"
+    # return {"hi there"}
+    prompt = request.input
+    response = generate_response(prompt)
+    print("response == ", response)
+    return response
+
