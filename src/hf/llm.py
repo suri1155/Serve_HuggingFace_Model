0659976d3b05282178c02fe91fcf0fcec7f2dbf5
Suraj <suri1155be@gmail.com>
2024-07-10 15:46:33 +0800

    Add files via upload
    updated

--- src/hf/llm.py
+++ src/hf/llm.py
@@ -4,8 +4,9 @@ from transformers import pipeline
 
 pipe = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
-def generate_response(prompt:str):
-   response = pipe("This is a great tutorial!")
-   label = response[0]["label"]
-   score = response[0]["score"]
-   return f"The '{prompt}' input is {label} with a score of {score}"
+
+def generate_response(prompt: str):
+    response = pipe(prompt)
+    label = response[0]["label"]
+    score = response[0]["score"]
+    return f"The '{prompt}' input is {label} with a score of {score}"
